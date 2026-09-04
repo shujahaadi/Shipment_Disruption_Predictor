0eef2986acda7eb3bbbaa0b3e0e3f014c8062d3e
Shuja <shuja04haadi@gmail.com>
2026-09-05 01:35:34 +0530

    Little updates

--- src/predict.py
+++ src/predict.py
@@ -6,59 +6,45 @@ model = load("models/decision_tree.pkl")
 preprocessor = load("models/preprocessor.pkl")
 
 
-# Example new shipment
-new_shipment = pd.DataFrame([{
-    "Order_Date": "2024-06-15",
-    "Origin_City": "Mumbai, IN",
-    "Destination_City": "Dubai, AE",
-    "Route_Type": "Direct",
-    "Transportation_Mode": "Air",
-    "Product_Category": "Electronics",
-    "Base_Lead_Time_Days": 10,
-    "Scheduled_Lead_Time_Days": 12,
-    "Geopolitical_Risk_Index": 0.3,
-    "Weather_Severity_Index": 2,
-    "Inflation_Rate_Pct": 4.5,
-    "Shipping_Cost_USD": 5000,
-    "Order_Weight_Kg": 200
-}])
+def predict_shipment(shipment):
+    new_shipment = pd.DataFrame([shipment])
 
 
-new_shipment["Order_Date"] = pd.to_datetime(
-    new_shipment["Order_Date"]
-)
+    new_shipment["Order_Date"] = pd.to_datetime(
+        new_shipment["Order_Date"]
+    )
 
-new_shipment["Order_Month"] = (
-    new_shipment["Order_Date"].dt.month
-)
+    new_shipment["Order_Month"] = (
+        new_shipment["Order_Date"].dt.month
+    )
 
-new_shipment["Order_DayOfWeek"] = (
-    new_shipment["Order_Date"].dt.dayofweek
-)
+    new_shipment["Order_DayOfWeek"] = (
+        new_shipment["Order_Date"].dt.dayofweek
+    )
 
 
-X_new = new_shipment[[
-    "Origin_City",
-    "Destination_City",
-    "Route_Type",
-    "Transportation_Mode",
-    "Product_Category",
-    "Base_Lead_Time_Days",
-    "Scheduled_Lead_Time_Days",
-    "Geopolitical_Risk_Index",
-    "Weather_Severity_Index",
-    "Inflation_Rate_Pct",
-    "Shipping_Cost_USD",
-    "Order_Weight_Kg",
-    "Order_Month",
-    "Order_DayOfWeek"
-]]
+    X_new = new_shipment[[
+        "Origin_City",
+        "Destination_City",
+        "Route_Type",
+        "Transportation_Mode",
+        "Product_Category",
+        "Base_Lead_Time_Days",
+        "Scheduled_Lead_Time_Days",
+        "Geopolitical_Risk_Index",
+        "Weather_Severity_Index",
+        "Inflation_Rate_Pct",
+        "Shipping_Cost_USD",
+        "Order_Weight_Kg",
+        "Order_Month",
+        "Order_DayOfWeek"
+    ]]
 
 
 
-X_new_processed = preprocessor.transform(X_new)
+    X_new_processed = preprocessor.transform(X_new)
 
 
-prediction = model.predict(X_new_processed)
+    prediction = model.predict(X_new_processed)
 
-print("Predicted Delivery Status:", prediction[0])
+    return prediction[0]
--- src/train_model.py
+++ src/train_model.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from joblib import dump
+from sklearn.metrics import classification_report, confusion_matrix
 
 from preprocess import prepare_data
 
@@ -27,14 +28,19 @@ X_test_processed = preprocessor.transform(X_test)
 
 
 model = DecisionTreeClassifier(
-    max_depth=10,
-    min_samples_leaf=10,
     random_state=42
 )
 
 
 model.fit(X_train_processed, y_train)
 
+y_pred = model.predict(X_test_processed)
+
+print("\nClassification Report:")
+print(classification_report(y_test, y_pred))
+
+print("Confusion Matrix:")
+print(confusion_matrix(y_test, y_pred))
 
 dump(model, "models/decision_tree.pkl")
 dump(preprocessor, "models/preprocessor.pkl")
